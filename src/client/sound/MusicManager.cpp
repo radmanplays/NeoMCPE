9dda27a873a63ee235118191dd41590454c947d1
radmanplays <95340057+radmanplays@users.noreply.github.com>
2026-06-09 12:27:06 +0330

    Update MusicManager.cpp

--- src/client/sound/MusicManager.cpp
+++ src/client/sound/MusicManager.cpp
@@ -213,7 +213,10 @@ void MusicManager::playOnAndroid(int index) {
 
     BinaryBlob blob;
     if (m_minecraft && m_minecraft->platform()) {
-        blob = m_minecraft->platform()->readAssetFile(track.filePath);
+        std::string assetPath = track.filePath;
+        if (assetPath.substr(0, 5) == "data/") assetPath = assetPath.substr(5);
+        assetPath = "assets/" + assetPath;
+        blob = m_minecraft->platform()->readAssetFile(assetPath);
     }
     if (!blob.data || blob.size <= 0) {
         LOGE("Music: cannot load asset %s (size=%d)\n", track.filePath.c_str(), blob.size);
