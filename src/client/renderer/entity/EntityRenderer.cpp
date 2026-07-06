b62bab3f04325783b15b796f69043efa1d07df23
radmanplays <95340057+radmanplays@users.noreply.github.com>
2026-07-06 20:09:42 +0330

    fix shadow bug

--- src/client/renderer/entity/EntityRenderer.cpp
+++ src/client/renderer/entity/EntityRenderer.cpp
@@ -258,7 +258,7 @@ void EntityRenderer::renderShadow(Entity* e, float x, float y, float z, float po
 	//Textures* textures = entityRenderDispatcher->textures;
 	//textures->bind(textures->loadTexture("%clamp%/environment/shadow.png"));
 	Textures* textures = entityRenderDispatcher->textures;
-	textures->loadAndBindTexture("/misc/shadow.png"); 
+	textures->loadAndBindTexture("misc/shadow.png"); 
     
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
