42c6f743bcba5e51d469c489d26dcde812d92f43
radmanplays <95340057+radmanplays@users.noreply.github.com>
2026-07-21 14:58:40 +0330

    Update LevelRenderer.cpp

--- src/client/renderer/LevelRenderer.cpp
+++ src/client/renderer/LevelRenderer.cpp
@@ -185,12 +185,8 @@ void LevelRenderer::allChanged()
 
 	lastFogType = mc->options.getIntValue(OPTIONS_FOG_TYPE);
 
-#ifndef __APPLE__
 	bool tint = mc->options.getBooleanValue(OPTIONS_FOLIAGE_TINT);
 	bool sideTint = mc->options.getBooleanValue(OPTIONS_TINTED_SIDE);
-#else 
-	bool tint = false, sideTint = false;
-#endif
 	FoliageColor::setUseTint(tint);
 	GrassColor::setUseTint(tint);
 	TileRenderer::setUseTint(sideTint);
